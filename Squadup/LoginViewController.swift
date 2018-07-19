6f0feb2149dba2e9e7daa81eeebd132c7bd0284b
Jun Hee Han <junhee071@gmail.com>
2018-07-19 09:47:05 -0700

    refactoring stringly typed constants

--- Squadup/LoginViewController.swift
+++ Squadup/LoginViewController.swift
@@ -66,13 +66,32 @@ extension LoginViewController: FUIAuthDelegate {
         guard let user = authDataResult?.user
             else { return }
         
-        let userRef = Database.database().reference().child("users").child(user.uid)
-        userRef.observeSingleEvent(of: .value, with: { [unowned self] (snapshot) in
-            if let user = User(snapshot: snapshot) {
-                print("Welcome back, \(user.username).")
-            } else {
-                self.performSegue(withIdentifier: "toCreateUsername", sender: self)
+        UserService.show(forUID: user.uid) { (user) in
+            if let user = user {
+                User.setCurrent(user)
+                let storyboard = UIStoryboard(name: "Main", bundle: .main)
+                if let initialViewController = storyboard.instantiateInitialViewController() {
+                    self.view.window?.rootViewController = initialViewController
+                    self.view.window?.makeKeyAndVisible()
+                    }
+                } else {
+                    self.performSegue(withIdentifier: "toCreateUsername", sender: self)
+                }
             }
-        })
-    }
+        }
+//        let userRef = Database.database().reference().child("users").child(user.uid)
+//        userRef.observeSingleEvent(of: .value, with: { [unowned self] (snapshot) in
+//            if let user = User(snapshot: snapshot) {
+//                User.setCurrent(user)
+//
+//                let storyboard = UIStoryboard(name: "Main", bundle: .main)
+//                if let initialViewController = storyboard.instantiateInitialViewController() {
+//                    self.view.window?.rootViewController = initialViewController
+//                    self.view.window?.makeKeyAndVisible()
+//                }
+//            } else {
+//                self.performSegue(withIdentifier: "toCreateUsername", sender: self)
+//            }
+//        })
 }
+
