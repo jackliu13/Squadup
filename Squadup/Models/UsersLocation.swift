de03cc436b3fc56e0e6fb79a9c3b1808d0978d9d
Ishaan Chandra <ishaanc3@gmail.com>
2018-07-19 09:58:36 -0700

    get Users location

---
+++ Squadup/Models/UsersLocation.swift
@@ -0,0 +1,16 @@
+//
+//  UsersLocation.swift
+//  Squadup
+//
+//  Created by Ishaan Chandra on 7/19/18.
+//  Copyright © 2018 Jack Liu. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class UsersLocation: NSObject {
+
+    var friendsLon: CLLocation?
+    var friendsLat: CLLocation?
+}
